c76ccbf8337a8282088c46df2e863a94abdeb79d
Kendaluski <j.m.jaenmontesino@gmail.com>
2023-06-07 07:22:56 -0700

    first 2 exercises

---
+++ Curso1/Practica_2.py
@@ -0,0 +1,6 @@
+# variable int
+numeros = 123456789
+# variable str
+string = "chema"
+print(numeros)
+print(string)
---
+++ Curso1/Practica_3.py
@@ -0,0 +1,2 @@
+nombre, nombre1, nombre2 = "Chema", "Juan", "Antonio"
+print(nombre, nombre1, nombre2)
